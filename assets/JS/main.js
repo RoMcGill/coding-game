eec90e237eb4a21a03bcab2f9df694b28a1509c9
RoMcGill <ro.mcgill12@gmail.com>
2022-02-21 13:43:57 +0000

    add basic js to show random phrase from string

---
+++ assets/JS/main.js
@@ -0,0 +1,40 @@
+window.addEventListener('load',init)
+//global variables
+let time = 5;
+let score= 0;
+let runGame;
+
+//DOM elemnets
+const wordInput = document.querySelector('#text-input');
+const currentPhrase = document.querySelector('#current-phrase');
+const socreDisplay = document.querySelector('#score');
+const timeDisplay = document.querySelector('#time');
+const message = document.querySelector('#message');
+const seconds = document.querySelector('#seconds');
+
+
+const phrases = [
+    '<h1>My First Heading</h1>',
+    '<p>My first paragraph.</p>',
+    '<a id=logo><img src=images/logo.gifalt=Logo</a>',
+    '<ul><li><a href=>Home</a></li><li><a href=>About</a></li><li><a href=>Services</a></li><li><a href=>Contact</a></li></ul> ',
+    '<h1>Really big title 2.0</h1>',
+    '<h2>Simple, yet engaging and descriptive subtitle</h2>',
+    '<div id="call-to-action"><a href="#" class="btn btn-large">Primary call to  action</a>',
+
+];
+
+//initialize game
+function init (){
+    // load phrase from array
+    showPhrase(phrases);
+
+}
+//pick and show random phrase 
+function showPhrase(phrases){
+    // generate random index
+    const randIndex = Math.floor(Math.random() * phrases.lenght);
+    // output random phrase
+    currentPhrase.innerHTML = phrases[randIndex];
+
+}
