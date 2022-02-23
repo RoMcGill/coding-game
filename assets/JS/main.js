cd94ac4e77d0087c42f35adc98c58bc3cbbdab99
RoMcGill <ro.mcgill12@gmail.com>
2022-02-23 16:26:56 +0000

    tutor support add dom reflow to main.js

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -33,11 +33,16 @@ const seconds = document.querySelector('#seconds');
 const lines =
 [
 
-    '<h1>My First Heading</h1>',
-    '<p>My first paragraph.</p>',
-    '<h1>Really big title 2.0</h1>',
-    '<h2>Simple, yet engaging and descriptive subtitle</h2>',
-    '<div id="call-to-action"><a href="#" class="btn btn-large">Primary call to  action</a>',
+    '<h1>Heading</h1>',
+    '<p>Paragraph</p>',
+    '<h1>big title</h1>',
+    '<h5>small title</h5>',
+    '<h2>descriptive subtitle</h2>',
+    '<div id="call">call div</div>',
+    '<span>this is a span</span>',
+    '<article> this is an article </article>',
+    '<ol><li>list item 1</li></ol>',
+
 
 ];
 
@@ -50,6 +55,7 @@ function init (){
     // call countdown every second
     setInterval(countdown, 1000);
     setInterval(checkStatus, 50);
+   
 
 }
 //start match
@@ -61,6 +67,9 @@ function startMatch() {
        showLine(lines);
        textInput.value = '';
        score ++;
+       setTimeout(currentLine.classList.add("linedown"),100)
+    
+
     }
     scoreDisplay.innerText = score;
 }
@@ -68,6 +77,13 @@ function startMatch() {
 function matchLines (){
     if (textInput.value === currentLine.innerText) {
         message.innerText = 'Correct'
+        currentLine.classList.remove("linedown") 
+
+        // trigger a DOM reflow 
+        void currentLine.offsetWidth; 
+        
+        currentLine.classList.add("linedown");
+        
         return true;
       let lineDown = document.getElementById("linedown")
      ///// problem trying to get line down animatioon to reset whern correct answer typed 
@@ -84,14 +100,17 @@ function matchLines (){
 function showLine(lines) {
     // Generate random array index
     const randIndex = Math.floor(Math.random() * lines.length);
-    // Output random word
+    // Output random line
     currentLine.innerText = lines[randIndex];
+
   }
+  
  //countdown timer
  function countdown() {
      // make sure time is >0
      if(time>0){
-         time--;
+         time--; 
+       
 
      } else if (time===0) {
 
@@ -100,7 +119,7 @@ function showLine(lines) {
      }
      timeDisplay.innerHTML = time
  }
-
+ 
 //check game status
 function checkStatus(){
     if (!runGame && time === 0) {
